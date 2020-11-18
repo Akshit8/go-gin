a11ec896d823df25dd808c93e4843dd560c96875
Akshit Sadana <akshitsadana@gmail.com>
2020-11-19 00:12:16 +0530

    handlers'

--- basic/main.go
+++ basic/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-	"net/http",
+	"net/http"
+	"fmt"
 	"log"
+	"io/ioutil"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	// have deafult route behaviour
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		// for logging to console
-		log.Println("Hello World")
+		log.Println("Hello World!")
+		d, _ := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "error", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s\n", d)
+	})
+
+	http.HandleFunc("/api", func(http.ResponseWriter, *http.Request) {
+		log.Println("api is hit")
 	})
 
 	http.ListenAndServe(":8000", nil)
