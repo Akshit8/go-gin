f48d06416cbada6dfc0a39f6ebdb47109f940aaf
Akshit Sadana <akshitsadana@gmail.com>
2020-11-19 19:55:46 +0530

    indent

--- basic/main.go
+++ basic/main.go
@@ -11,10 +11,10 @@ import (
 
 // Book struct (Model)
 type Book struct {
-	ID		string	`json:"id"`
-	Isbn	string	`json:"isbn"`
-	Title	string	`json:"title"`
-	Author	*Author	`json:"author"`
+	ID      string	`json:"id"`
+	Isbn    string	`json:"isbn"`
+	Title   string	`json:"title"`
+	Author  *Author	`json:"author"`
 }
 
 // Author Struct
